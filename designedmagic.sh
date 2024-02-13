7b50229501d77328ca72c1817a53f6d1cdb989f4
Pravash Chandra Das <78121548+pravashdas@users.noreply.github.com>
2024-02-13 18:04:06 +0530

    Update designedmagic.sh
    changed design

--- designedmagic.sh
+++ designedmagic.sh
@@ -10,10 +10,8 @@
 #############################################
 
 echo "########################################################################################################################"
-echo "#                               Imagine a 2-digit number which is not divisible by 11.
-  #"
-echo "#                               Flip that number and subtract from greater to smaller.
-  #"
+echo "#                               Imagine a 2-digit number which is not divisible by 11.                                 #"
+echo "#                               Flip that number and subtract from greater to smaller.                                 #"
 echo -n "#                                         Give me the left-hand side digit:"
 
 
@@ -23,11 +21,8 @@ read left
 # Validate user input
 if [[ $left =~ ^[0-9]$ ]]; then
     right=$((9 - left))
-    echo "#                                           Your subtraction number is ${left}${right}.
-                   #"
+    echo "#                                           Your subtraction number is ${left}${right}.                            #"
 else
-    echo "#                                           Invalid input. Please enter a single digit.
-                   #"
+    echo "#                                           Invalid input. Please enter a single digit.                            #"
 fi
-
 echo "########################################################################################################################"
